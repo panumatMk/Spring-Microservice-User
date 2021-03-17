3361333d92b1f380fb37c3f6bee28d7b01890175
Yo <panumat.ma>
2021-03-17 22:46:21 +0700

    [update] change call service by url to call by  microservice

--- src/main/java/com/dailycodebuffer/user/service/UserService.java
+++ src/main/java/com/dailycodebuffer/user/service/UserService.java
@@ -28,7 +28,7 @@ public class UserService {
         log.info("Inside getUserWithDepartment of UserService");
         ResponseTemplateVo vo = new ResponseTemplateVo();
         User user = userRepository.findByUserId(userId);
-        Department department = restTemplate.getForObject("http://localhost:9001/departments/"+user.getDepartmentId(),Department.class);
+        Department department = restTemplate.getForObject("http://DEPARTMENT-SERVICE/departments/"+user.getDepartmentId(),Department.class);
 
         vo.setUser(user);
         vo.setDepartment(department);
