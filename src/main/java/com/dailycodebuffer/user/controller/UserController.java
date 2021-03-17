b8c8122b0b243c6b4a16f40c41f288462521a7bf
Yo <panumat.ma>
2021-03-17 20:52:33 +0700

    first commit

---
+++ src/main/java/com/dailycodebuffer/user/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.dailycodebuffer.user.controller;
+
+import com.dailycodebuffer.user.VO.ResponseTemplateVo;
+import com.dailycodebuffer.user.service.UserService;
+import com.dailycodebuffer.user.entity.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+@Slf4j
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/")
+    public User saveUser(@RequestBody User user){
+        log.info("Inside saveUser of UserController");
+        return userService.saveUser(user);
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseTemplateVo getUserWithDepartment(@PathVariable("id") Long userId){
+        log.info("Inside getUserWithDepartment of UserController");
+        return userService.getUserWithDepartment(userId);
+    }
+}
